a5f9e3f60789c9b1803932108f04755f7df9f8a7
BazhenovVS <bazhenov4job@gmail.com>
2025-02-05 13:09:15 +0300

    обновил поля, отвечающие за классы

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -5,10 +5,10 @@ data class Post(
     val fromId: Int = 0,
     var text: String = "",
     var friendsOnly: Boolean = false,
-    var comments: Comments,
-    val copyright: Copyright,
-    var likes: Likes,
-    var reposts: Reposts,
-    var views: Views,
+    var comments: Comments = Comments(),
+    val copyright: Copyright = Copyright(),
+    var likes: Likes = Likes(),
+    var reposts: Reposts = Reposts(),
+    var views: Views = Views(),
     val postType: String = "post"
 )

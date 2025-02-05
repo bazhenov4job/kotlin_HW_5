d234ffd7abdc3d0955454b5185278339c2580cd2
BazhenovVS <bazhenov4job@gmail.com>
2025-02-05 13:18:19 +0300

    добавил в тест очистку синглтона перед тестом

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -1,7 +1,25 @@
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 class WallServiceTest {
 
+    @Before
+    fun clearBeforeTest() {
+        WallService.clear()
+    }
+
+    @Test
+    fun updateExisting() {
+        val wallService = WallService
+
+        wallService.add(post=Post())
+        wallService.add(post=Post())
+        wallService.add(post=Post())
+
+        val result = wallService.update(post=Post(id=1))
+
+        assertTrue(result)
+    }
 }
